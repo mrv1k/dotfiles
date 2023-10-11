fb8d37ac016afd2520446373539c9e1fa3c0b7ce
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2023-10-10 20:28:19 -0400

    move config to init

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,9 @@
 -- this file is used for vim configs
 
+-- See `:help vim.o`
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+
 
 -- https://neovim.io/doc/user/lua.html#:~:text=LUA%2DVIMSCRIPT%20BRIDGE
 -- let g:netrw_list_hide= '.DS_Store'
@@ -23,24 +25,34 @@ vim.opt.wrap = false
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+-- Save undo history
 vim.opt.undofile = true
 
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
 
+-- Case-insensitive searching UNLESS \C or capital in search
 vim.opt.ignorecase = true
 vim.opt.smartindent = true
 vim.opt.smartcase = true
+-- NOTE: You should make sure your terminal supports this
 vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 20
+-- Keep signcolumn on by default
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@") -- allow "@" in path, why? dunno but hey Prime does it
 
-vim.opt.updatetime = 50
+-- Decrease update time
+vim.opt.updatetime = 150
+vim.o.timeoutlen = 300
 vim.opt.colorcolumn = "81"
 
---
+-- Enable break indent
+vim.o.breakindent = true
+-- Set completeopt to have a better completion experience
+-- The completion menu is controlled by completeopt. 
+vim.o.completeopt = 'menuone,noselect'
 
 -- :h lua-high
 local ag = vim.api.nvim_create_augroup
